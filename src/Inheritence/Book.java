b0809637aa89a559f3fa6b58b3496a4ce62c4059
EkhlasRayeesKhan <ekhlashrayees@gmail.com>
2026-02-13 10:46:19 +0200

    Created Fiction CLass

--- src/Inheritence/Book.java
+++
@@ -1,9 +0,0 @@
-package Inheritence;
-
-public class Book {
-        public String title;
-        public String author;
-        public String isbn;
-        public int pages;
-        public boolean isAvailable;
-}
--- src/Inheritence/FictionBook.java
+++
@@ -1,4 +0,0 @@
-package Inheritence;
-
-public class FictionBook {
-}
