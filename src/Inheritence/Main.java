991ca07da69c687cc76f900530d66f45aae4af6f
EkhlasRayeesKhan <ekhlashrayees@gmail.com>
2026-02-13 11:53:41 +0200

    Added Method(4,5,6) to Lfi

--- src/Inheritence/Main.java
+++ src/Inheritence/Main.java
@@ -8,5 +8,7 @@ public class Main {
         librarySystem.addBookToSystem(new Fiction("Harry Potter", "J.K. Rowling", "1234", 500, "Fantasy"));
         librarySystem.addBookToSystem(new NonFiction("A Brief History of Time ", "Stephen Hawking", "5678", 200,  "Science"));
         librarySystem.addBookToSystem(new Comic("Batman: Year One", "Frank Miller", "9101", 150, "David Mazzucchelli"));
+        librarySystem.addMemberToSystem(new Member("Alice", "M001"));
+        librarySystem.addMemberToSystem(new Member("Bob", "M002"));
     }
 }
