878ba62b6673708c47cf3796bef6f9c220f5ceb9
Ilknur Rahimli <ilknur.ragimly@gmail.com>
2026-02-13 11:42:17 +0200

    updated main class

--- src/Inheritence/Main.java
+++ src/Inheritence/Main.java
@@ -2,5 +2,11 @@ package Inheritence;
 
 public class Main {
 
-
+    public static void main(String[] args) {
+        LibrarySystem librarySystem = new LibrarySystem();
+        librarySystem.librarySystem();
+        librarySystem.addBookToSystem(new Fiction("Harry Potter", "J.K. Rowling", "1234", 500, "Fantasy"));
+        librarySystem.addBookToSystem(new NonFiction("A Brief History of Time ", "Stephen Hawking", "5678", 200,  "Science"));
+        librarySystem.addBookToSystem(new Comic("Batman: Year One", "Frank Miller", "9101", 150, "David Mazzucchelli"));
+    }
 }
